ea473b99d61de58f88ba331647739d2e8e1987ff
Eduard Farinyes <efarinyes@gmail.com>
2024-01-19 21:33:51 +0100

    NOC app - final seccio 8

---
+++ src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,37 @@
+
+import axios from 'axios';
+
+
+interface CheckServiceUseCase {
+    execute( url: string): Promise<boolean>
+}
+
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
+export class CheckService implements CheckServiceUseCase {
+
+    constructor(
+        private readonly successCallback: SuccessCallback,
+        private readonly errorCallback: ErrorCallback
+        
+    ) {}
+
+   public async execute( url: string): Promise<boolean> {
+
+    try {
+        const req = await axios.get(url);
+        if ( !req ) {
+            throw new Error( `Error on check service ${ url }` );
+        }
+        this.successCallback();
+        
+        return true;
+    } catch (error) {
+        this.errorCallback(`${error}`);
+        console.log(`${ error }`);
+        
+        return false;
+    }
+   }
+}
